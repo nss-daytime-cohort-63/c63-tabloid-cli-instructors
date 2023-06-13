18040caf9d7b1419e9b6027e1168ab493d7b729f
Valerie Freeman <valerah7@gmail.com>
2023-06-13 15:12:34 -0500

    added post manager and functionality to list posts

---
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class PostManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private PostRepository _postRepository;
+
+        public PostManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _postRepository = new PostRepository(connectionString);
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Post Menu");
+            Console.WriteLine(" 1) List Posts");
+            Console.WriteLine(" 2) Add Post");
+            Console.WriteLine(" 3) Edit Post");
+            Console.WriteLine(" 4) Remove post");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "2":
+                    Add();
+                    return this;
+                case "3":
+                    Edit();
+                    return this;
+                case "4":
+                    Remove();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            Console.WriteLine("Current posts: ");
+            List<Post> posts = _postRepository.GetAll();
+            foreach (Post p in posts)
+            {
+                Console.WriteLine($"{p.Id} )  {p.Title} : {p.Url}");
+            }
+            Console.WriteLine("");
+        }
+
+        private void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Remove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
