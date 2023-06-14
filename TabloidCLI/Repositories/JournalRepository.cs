ca85dc4689972225b80b1a06cd425a6d943d1680
Valerie Freeman <valerah7@gmail.com>
2023-06-14 11:21:04 -0500

    implemented journal manager and list journal entries

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.Repositories
+{
+    internal class JournalRepository : DatabaseConnector, IRepository<JournalEntry>
+    {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+
+        public List<JournalEntry> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Title, Content, CreateDateTime FROM Journal";
+                    List<JournalEntry> entries = new List<JournalEntry>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        JournalEntry entry = new JournalEntry()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                            
+                        };
+                        entries.Add(entry);
+                    }
+
+                    reader.Close();
+
+                    return entries;
+                }
+            }
+        }
+
+        public JournalEntry Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(JournalEntry post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(JournalEntry post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    internal class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 1) List Entries");
+            Console.WriteLine(" 2) Add Entry");
+            Console.WriteLine(" 3) Edit Entry");
+            Console.WriteLine(" 4) Remove Entry");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "2":
+                    Add();
+                    return this;
+                case "3":
+                    Edit();
+                    return this;
+                case "4":
+                    Remove();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            Console.WriteLine("Previous entries: ");
+            List<JournalEntry> entries = _journalRepository.GetAll();
+            foreach (JournalEntry e in entries)
+            {
+                Console.WriteLine($"\t{e.Title} : {e.CreateDateTime.ToShortDateString()}\n\t\t{e.Content}");
+            }
+            Console.WriteLine("");
+        }
+
+        private void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Remove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
